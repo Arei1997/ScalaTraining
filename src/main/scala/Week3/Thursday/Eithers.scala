d384b10ae841b8bc82cc1d222e7693219e7debdc
Arei <98773616+Arei1997@users.noreply.github.com>
2024-07-20 00:57:37 +0100

    Added part 8

--- src/main/scala/Week3/Thursday/Eithers.scala
+++ src/main/scala/Week3/Thursday/Eithers.scala
@@ -1,70 +1,70 @@
-package Week3.Thursday
+object Extension extends App {
 
-import sun.security.util.Password
+  case class Name(value: String)
+  case class Postcode(value: String)
 
-import java.time.LocalDate
+  case class Letter(name: Name,postcode: Postcode)
 
-object Eithers extends App {
-
-
-  //Left - error
-  //Right - success
-
-  def isEven(x: Int): Either[String, String] =
-    if (x % 2 == 0) Right(s"$x is even") else Left(s"$x is odd")
-
-  println(isEven(4))
-
-
-  //custom error
-  class NewError(val errorCode: Int, val errorMessage: String) extends Exception
-
-  case object isOddError extends NewError(400, "Bsd request in the is off method")
-
-  def isOdd(x: Int): Either[NewError, String] =
-    if (x % 2 != 0) Right(s"$x is odd") else Left(isOddError)
-
-  println(isOdd(4))
-  println(isOdd(-1))
-
-
-  case class User(username: String, password: String, dateOfBirth: LocalDate)
-
-  case class ValidatedUser(username: String, password: String, dateOfBirth: LocalDate)
-  //validate the user
-
-  def validateUsername(username: String): Either[String, String] = {
-    if (username.length < 12) Left("Username must be more than 12 characters")
-    else Right(username)
-
+  trait genericPostageError{
+    def message: String
   }
 
-  //validate the password
-  def validatePassword(password: String): Either[String, String] = {
-    if (password.toUpperCase == password || password.toLowerCase == password) Left("password must be a mixture of both")
-    else Right(password)
+  case class invalidNameError(message:String) extends genericPostageError
+
+  case class invalidPostcodeError(message:String) extends genericPostageError
+
+
+  // sample data
+  val letter:Either[genericPostageError,Letter] = Right(Letter(name=Name("David"),postcode = Postcode("AB2 CD3")))
+  val invalidLetter:Either[genericPostageError,Letter] = Left(invalidNameError("Invalid Name Given"))
+  val invalidPostcode: Either[genericPostageError, Letter] = Left(invalidPostcodeError("Invalid Postcode Given"))
+  val validPostcodeLetter: Either[genericPostageError, Letter] = Right(Letter(name = Name("Alice"), postcode = Postcode("VALID123")))
+
+
+  letter match {
+    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
+    case Left(y) => println(s"Error: ${y.message}")
   }
 
-  //validate the age
-
-  def validateAge(dateOfBirth:LocalDate): Either[String,LocalDate] = {
-    if (dateOfBirth.plusYears(18).isAfter(LocalDate.now)) Left("user must be at least 18") else Right(dateOfBirth)
+  invalidLetter match {
+    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
+    case Left(y) => println(s"Error: ${y.message}")
   }
 
-  //combine
-  def validateUser(user: User): Either[String,ValidatedUser] =
-    for {
-      username <- validateUsername(user.username)
-      password <- validatePassword(user.password)
-      dateOfBirth      <- validateAge(user.dateOfBirth)
-      createValidateUser = ValidatedUser(username,password, dateOfBirth)
+  invalidPostcode match {
+    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
+    case Left(y) => println(s"Error: ${y.message}")
+  }
 
-    }yield createValidateUser
+  validPostcodeLetter match {
+    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
+    case Left(y) => println(s"Error: ${y.message}")
+  }
 
-  val usernameBad = User("april", "Password", LocalDate.of(1994,3,30))
+object Postcode {
+  def postcodeOrError(maybePostCode:String):Either[invalidPostcodeError,Postcode] = {
+    val parts = maybePostCode.trim.split(" ")
 
-  println(validateUser(usernameBad))
+
+    if (parts.length == 2) {
+      Right(Postcode(maybePostCode))
+    }else {
+      Left(invalidPostcodeError("Invalid postcode"))
+    }
+  }
+}
+  val testPostcode1 = "AB2 CD3"
+  val testPostcode2 = "88AA8A8A8"
+
+  Postcode.postcodeOrError(testPostcode2) match {
+    case Left(error) =>println(s"Error: ${error.message}")
+    case Right(postcode) => println(s"Valid postcode entered: ${postcode.value}")
+  }
+
+  Postcode.postcodeOrError(testPostcode1) match {
+    case Left(error) =>println(s"Error: ${error.message}")
+    case Right(postcode) => println(s"Valid postcode entered: ${postcode.value}")
+  }
+
 
 }
-
-
