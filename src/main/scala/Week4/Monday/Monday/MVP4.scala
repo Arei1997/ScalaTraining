199d7ad9faaa577d259680db7c3cbc4e17b75ed6
Arei Mohammed <Arei1997@hotmail.co.uk>
2024-07-23 00:21:30 +0100

    MVP4

---
+++ src/main/scala/Week4/Monday/Monday/MVP4.scala
@@ -0,0 +1,19 @@
+package Week4.Monday.Monday
+
+import scala.annotation.tailrec
+
+object MVP4 extends App{
+
+  def Sum(n:Int):Int = {
+    @tailrec
+    def sumHelper(x:Int,acc:Int) : Int = {
+      if (x == 0) acc
+      else sumHelper(x-1, x+acc)
+    }
+    sumHelper(n,0)// n goes into x and 0 is the acc value. n ==> x , if n = 5 , then pass to x =5 etc
+  }
+
+  val number = 1
+  println(Sum(number))
+
+}
