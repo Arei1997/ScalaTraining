1f4432dd21c3dbc1bfdda31972b22006ac10d8b0
Arei <98773616+Arei1997@users.noreply.github.com>
2024-07-22 01:50:44 +0100

    Update Eithers.scala

--- src/main/scala/Week3/Thursday/Eithers.scala
+++ src/main/scala/Week3/Thursday/Eithers.scala
@@ -1,76 +1,51 @@
 object Extension extends App {
 
   case class Name(value: String)
-  case class Postcode(value: String)
-  case class Letter(name: Name,postcode: Postcode)
 
-  trait genericPostageError{
+  case class Postcode(value: String)
+
+  case class Letter(name: Name, postcode: Postcode)
+
+  trait genericPostageError {
     def message: String
   }
 
-  case class invalidNameError(message:String) extends genericPostageError
+  case class invalidNameError(message: String) extends genericPostageError
 
-  case class invalidPostcodeError(message:String) extends genericPostageError
-
-
-  // sample data
-  val letter:Either[genericPostageError,Letter] = Right(Letter(name=Name("David"),postcode = Postcode("AB2 CD3")))
-  val invalidLetter:Either[genericPostageError,Letter] = Left(invalidNameError("Invalid Name Given"))
-  val invalidPostcode: Either[genericPostageError, Letter] = Left(invalidPostcodeError("Invalid Postcode Given"))
-  val validPostcodeLetter: Either[genericPostageError, Letter] = Right(Letter(name = Name("Alice"), postcode = Postcode("VALID123")))
+  case class invalidPostcodeError(message: String) extends genericPostageError
 
 
-  letter match {
-    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
-    case Left(y) => println(s"Error: ${y.message}")
-  }
-
-  invalidLetter match {
-    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
-    case Left(y) => println(s"Error: ${y.message}")
-  }
-
-  invalidPostcode match {
-    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
-    case Left(y) => println(s"Error: ${y.message}")
-  }
-
-  validPostcodeLetter match {
-    case Right(x) => println(s"Letter: Name = ${x.name.value}, Postcode = ${x.postcode.value}")
-    case Left(y) => println(s"Error: ${y.message}")
-  }
-
-object Postcode {
-  def postcodeOrError(maybePostCode:String):Either[invalidPostcodeError,Postcode] = {
-    val parts = maybePostCode.trim.split(" ")
+  object Postcode {
+    def postcodeOrError(maybePostCode: String): Either[invalidPostcodeError, Postcode] = {
+      val parts = maybePostCode.trim.split(" ")
 
 
-    if (parts.length == 2) {
-      Right(Postcode(maybePostCode))
-    }else {
-      Left(invalidPostcodeError("Invalid postcode"))
+      if (parts.length == 2) {
+        Right(Postcode(maybePostCode))
+      } else {
+        Left(invalidPostcodeError("Invalid postcode"))
+      }
     }
   }
-}
 
 
   object Name {
-    def nameOrError(maybeName:String):Either[invalidNameError,Name] ={
+    def nameOrError(maybeName: String): Either[invalidNameError, Name] = {
       val digitCheck = ".*\\d.*".r
-      maybeName match{
+      maybeName match {
         case digitCheck() => Left(invalidNameError("Contains Numbers"))
         case _ => Right(Name(maybeName))
       }
     }
   }
 
+}
 
-
-  object Letter {
-    def nameOrError(name:Name,postcode: Postcode): Either[genericPostageError, Letter] = {
-      val maybeName= nameOrError(name,postcode)
-    }
-
+object Letter {
+  def nameOrError(name:Name,postcode: Postcode): Either[genericPostageError, Letter] = {
+    val maybeName= nameOrError(name,postcode)
   }
+
+}
 }
 
