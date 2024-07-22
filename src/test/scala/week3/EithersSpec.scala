d08558b820aad542ccfdff82623eb8d8dca89a9a
Arei <98773616+Arei1997@users.noreply.github.com>
2024-07-22 01:35:57 +0100

    Update EithersSpec.scala

--- src/test/scala/week3/EithersSpec.scala
+++ src/test/scala/week3/EithersSpec.scala
@@ -1,26 +1,46 @@
-package week3
-import Week3.Thursday.Eithers._
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class EithersSpec extends AnyWordSpec with Matchers{
+class ExtensionSpec extends AnyFlatSpec with Matchers {
+  import Extension._
 
-  "isOdd" should {
-    "return a Right" when {
-     "the number is odd " in {
-       val result = isOdd(3)
-       result shouldBe Right("3 is odd")
-     }
-      "the number is off and large" in {
-        val result = isOdd(100001)
-        result shouldBe Right("100001 is odd")
-      }
-
-    }
+  "Name validation" should "pass for valid names" in {
+    Name.nameOrError("David") shouldEqual Right(Name("David"))
   }
 
+  it should "fail for names containing numbers" in {
+    Name.nameOrError("David123") shouldEqual Left(invalidNameError("Contains Numbers"))
+  }
 
+  it should "pass for names with spaces" in {
+    Name.nameOrError("David Smith") shouldEqual Right(Name("David Smith"))
+  }
 
+  it should "pass for names with special characters but no numbers" in {
+    Name.nameOrError("David-Smith") shouldEqual Right(Name("David-Smith"))
+  }
 
+  it should "fail for names containing both numbers and special characters" in {
+    Name.nameOrError("David-Smith123") shouldEqual Left(invalidNameError("Contains Numbers"))
+  }
+
+  it should "pass for empty names" in {
+    Name.nameOrError("") shouldEqual Right(Name(""))
+  }
+
+  "Postcode validation" should "pass for valid postcodes" in {
+    Postcode.postcodeOrError("AB2 CD3") shouldEqual Right(Postcode("AB2 CD3"))
+  }
+
+  it should "fail for invalid postcodes" in {
+    Postcode.postcodeOrError("88AA8A8A8") shouldEqual Left(invalidPostcodeError("Invalid postcode"))
+  }
+
+  it should "pass for valid postcodes with leading and trailing spaces" in {
+    Postcode.postcodeOrError("AB2 CD3") shouldEqual Right(Postcode("AB2 CD3"))
+  }
+
+  it should "fail for empty postcodes" in {
+    Postcode.postcodeOrError("") shouldEqual Left(invalidPostcodeError("Invalid postcode"))
+  }
 }
-
